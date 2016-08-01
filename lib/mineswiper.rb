491f098d797b81b398df145b02f9edf22ef4138e
BranLiang <lby89757@hotmail.com>
2016-08-01 12:23:53 +0800

    finish the tile

--- lib/mineswiper.rb
+++ lib/mineswiper.rb
@@ -1,12 +1,29 @@
 require "mineswiper/version"
 require "mineswiper/board"
-require "mineswiper/cell"
+require "mineswiper/tile"
 require 'mineswiper/player'
+require 'mineswiper/display'
 require 'colorize'
 
 module Mineswiper
   class Game
 
-    
+    attr_accessor :board, :name, :player
+
+    def initialize(name: "Player1", board: Board.new)
+      @name = name
+      @board = board
+      @board.prepared_board
+      @player = Player.new(@board)
+    end
+
+    def play
+      until game_over?
+        board.parse_input(player.move)
+      end
+    end
+
+
+
   end
 end
